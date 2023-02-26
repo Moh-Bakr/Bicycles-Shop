18b1a273b330ba67bcf4a7b9f2fbdc7e4a009c7a
Mohamed Bakr <moh.bakr27@gmail.com>
2023-02-26 15:56:29 +0200

    authentication using jwt

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest
   validates :email, uniqueness: true
 
+  PASSWORD_REQUIREMENTS = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x
+
+  validates :password, format: { with: PASSWORD_REQUIREMENTS, message: 'must contain at least one lowercase letter, one uppercase letter, one digit, and one special character' }
+
   has_secure_password
 end
